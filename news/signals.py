7ff991e7b6d6ce91628985933fe811dea306b76e
antluc89 <antluc89@yandex.by>
2024-09-13 07:30:23 -0500

    Init NewsPaper

--- news/signals.py
+++ news/signals.py
@@ -5,6 +5,8 @@ from django.dispatch import receiver
 from .models import PostCategory, Post
 from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
+from .tasks import send_email_task, weekly_send_mail_task
+from NewsPaper.NewsPaper import settings
 
 
 @receiver(m2m_changed, sender=PostCategory)
@@ -12,23 +14,25 @@ def post_created(instance, **kwargs):
     if not kwargs['action'] == 'post_add':
         return
 
-    emails = User.objects.filter(
-        subscriptions__category__in=instance.category.all()
-    ).values_list('email', flat=True)
+#    emails = User.objects.filter(
+#        subscriptions__category__in=instance.category.all()
+#    ).values_list('email', flat=True)
+#
+#    subject = f'Пост в категории {", ".join([str(cat) for cat in instance.category.all()])}'
+#
+#    text_content = (
+#        f'Название: {instance.name_post}\n'
+#        f'Ссылка на пост: http://127.0.0.1:8000{instance.get_absolute_url()}'
+#    )
+#    html_content = (
+#        f'Название: {instance.name_post}<br>'
+#        f'<a href="http://127.0.0.1{instance.get_absolute_url()}">'
+#        f'Ссылка на пост</a>'
+#    )
+#    for email in emails:
+#        msg = EmailMultiAlternatives(subject, text_content, None, [email])
+#        msg.attach_alternative(html_content, "text/html")
+#        msg.send()
 
-    subject = f'Пост в категории {", ".join([str(cat) for cat in instance.category.all()])}'
-
-    text_content = (
-        f'Название: {instance.name_post}\n'
-        f'Ссылка на пост: http://127.0.0.1:8000{instance.get_absolute_url()}'
-    )
-    html_content = (
-        f'Название: {instance.name_post}<br>'
-        f'<a href="http://127.0.0.1{instance.get_absolute_url()}">'
-        f'Ссылка на пост</a>'
-    )
-    for email in emails:
-        msg = EmailMultiAlternatives(subject, text_content, None, [email])
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
+    send_email_task.delay(instance.id)
 
---
+++ news/tasks.py
@@ -0,0 +1,64 @@
+from celery import shared_task
+from datetime import timezone, datetime
+from datetime import timedelta
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+from NewsPaper.NewsPaper import settings
+from NewsPaper.news.models import Post, Category
+
+
+@shared_task
+def send_email_task(pk):
+    post = Post.objects.get(pk=pk)
+    categories = post.category.all()
+    subscribers_email = []
+    for category in categories:
+        subscribers_users = category.subscribers.all()
+        for sub_users in subscribers_users:
+            subscribers_email.append(sub_users.email)
+    html_content = render_to_string(
+        'post_created_email.html',
+        {'text': f'{post.name_post}',
+         'link': f'{settings.SITE_URL}/news/{pk}',
+         }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject='Новости за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers_email
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+@shared_task
+def weekly_send_mail_task():
+    today = timezone.now()
+    last_week = today - datetime.timedelta(days=7)
+    news = Post.objects.filter(time_post__gte=last_week)
+    categories = set(news.values_list('category__name_category', flat=True))
+    subscribers = set(Category.objects.filter(category_name__in=categories).values_list('subscribers__email', flat=True))
+
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': settings.SITE_URL,
+            'news': news,
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject='Новости за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
